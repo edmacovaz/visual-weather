88cbb11ec30d2573d198f817682dc3830a6d085d
Pankrat <lhaehne@gmail.com>
2012-09-22 23:13:19 +0200

    Randomize image order

--- visual_weather/visual_weather/views.py
+++ visual_weather/visual_weather/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.views.decorators.cache import cache_page
 from datetime import date, timedelta
+from random import shuffle
 from base import photos
 from base import weather
 
@@ -25,6 +26,7 @@ def index(request):
     matching_date = weather.find_matches(current_data["data"], limit=1)[0].date
     year, month, day = matching_date.year, matching_date.month, matching_date.day
     urls = photos.search(year, month, day, lat=lat, lon=lon)
+    shuffle(urls)
 
     context = dict(urls=urls,
                    matching_date=matching_date,
@@ -38,6 +40,6 @@ def index(request):
 @cache_page(60*60)
 def for_day(request, day=''):
     year, month, day = day.split('-')
-    photos = photos.search(int(year), int(month), int(day))
-    return render_to_response("master.html", dict(photos=photos))
+    urls = photos.search(int(year), int(month), int(day))
+    return render_to_response("master.html", dict(urls=urls))
 
