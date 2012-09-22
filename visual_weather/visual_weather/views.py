19bde007705aada9d491f8b5bfa2779cab24c27d
Pankrat <lhaehne@gmail.com>
2012-09-22 21:47:37 +0200

    Remove cruft

--- visual_weather/visual_weather/views.py
+++ visual_weather/visual_weather/views.py
@@ -4,13 +4,14 @@ from datetime import date, datetime
 from base import photos
 from base import weather
 
+
 @cache_page(60*60)
 def index(request):
     lat, lon = 52.529531, 13.411978
     when = request.GET.get("when", date.today().strftime("%Y/%m/%d"))
     matching_date = weather.date_matching_weather(lat, lon, when=when)
     year, month, day = matching_date.year, matching_date.month, matching_date.day
-    urls = photos.search(year, month, day)
+    urls = photos.search(year, month, day, lat=lat, lon=lon)
     context = dict(urls=urls,
                    matching_date=matching_date)
     return render_to_response("master.html", context)
