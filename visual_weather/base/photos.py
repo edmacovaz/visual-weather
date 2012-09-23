f25486a879031009e9fcf740413aa138b66398d9
Pankrat <lhaehne@gmail.com>
2012-09-23 11:52:20 +0200

    Try to filter multiple photos by the same user

--- visual_weather/base/photos.py
+++ visual_weather/base/photos.py
@@ -7,7 +7,7 @@ from datetime import date, datetime, timedelta
 from base.cache import cache_result
 
 @cache_result()
-def search(year, month, day, lat=52.52992, lon=13.41157, limit=32):
+def search(year, month, day, lat=52.52992, lon=13.41157, limit=32, bust=1):
     """
     Return photos for the given date object.
     """
@@ -17,11 +17,25 @@ def search(year, month, day, lat=52.52992, lon=13.41157, limit=32):
                                   max_taken_date=end.strftime("%Y-%m-%d"),
                                   lat=str(lat),
                                   lon=str(lon),
-                                  sort='interestingness-desc',
+                                  #sort='interestingness-desc',
                                   #geo_context='2',
-                                  per_page=limit,
+                                  per_page=limit*2,
                                   radius='10')
-    return [(p.getMedium(), p.title) for p in photos]
+    final = []
+    users = set()
+    removed = 0
+    # Filter multiple photos by same user
+    for photo in photos:
+        if photo.owner.id not in users:
+            final.append(photo)
+            users.add(photo.owner.id)
+        elif len(photos) - removed <= limit:
+            # Don't remove if we'd end up with not enough photos
+            final.append(photo)
+        else:
+            removed += 1
+
+    return [(p.getMedium(), p.title) for p in final]
 
 
 def facebook_search(year, month, day):
